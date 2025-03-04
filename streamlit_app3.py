03f8c9ded600cefbd51051e5e45dbe67978b1601
reynacolney <70680885+reynacolney@users.noreply.github.com>
2025-03-03 20:12:02 -0700

    Update streamlit_app3.py

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -42,6 +42,6 @@ if ingredients_list:
     if time_to_insert:
       session.sql(my_insert_stmt).collect()
 
-       st.success('Your Smoothie is ordered, ' +name_on_order+'!')
+    st.success('Your Smoothie is ordered, ' +name_on_order+'!')
 
 
