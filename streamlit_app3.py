f1da2a215f69a675820426f519f932eec606ab9f
reynacolney <70680885+reynacolney@users.noreply.github.com>
2025-03-05 19:32:37 -0700

    Update streamlit_app3.py
    Further updates to search on

--- streamlit_app3.py
+++ streamlit_app3.py
@@ -16,8 +16,8 @@ my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT
 #st.dataframe(data=my_dataframe, use_container_width=True)
 #Convert the Snowpark dataframe to a Pandas dataframe fro LOC function
 pd_df= my_dataframe.to_pandas()
-st.dataframe(pd_df)
-st.stop()
+#st.dataframe(pd_df)
+#st.stop()
 
 ingredients_list=st.multiselect(
     'Choose up to 5 ingredients'
@@ -31,6 +31,10 @@ if ingredients_list:
 
     for fruit_chosen in ingredients_list:
          ingredients_string +=fruit_chosen + ' '
+        
+         search_on = pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+         st.write('The search value for ', fruit_chosen, ' is ', search_on, '.')
+        
          st.subheader(fruit_chosen + 'Nutrition Information')
          import requests
          smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+fruit_chosen)
